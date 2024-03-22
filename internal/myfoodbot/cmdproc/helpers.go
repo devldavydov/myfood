d46a5787bae1792bdf3894eb5ccb0a68c1150214
devldavydov <devldavydov@yandex.ru>
2024-03-22 12:04:03 +0300

    Fix empty date

--- internal/myfoodbot/cmdproc/helpers.go
+++ internal/myfoodbot/cmdproc/helpers.go
@@ -5,16 +5,11 @@ import (
 )
 
 func parseTimestamp(sTimestamp string) (int64, error) {
-	if sTimestamp == "" {
-		t := time.Now()
-		return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, time.UTC).Unix(), nil
-	} else {
-		t, err := time.Parse("02.01.2006", sTimestamp)
-		if err != nil {
-			return 0, err
-		}
-		return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, time.UTC).Unix(), nil
+	t, err := time.Parse("02.01.2006", sTimestamp)
+	if err != nil {
+		return 0, err
 	}
+	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, time.UTC).Unix(), nil
 }
 
 func formatTimestamp(tsUnix int64) string {
