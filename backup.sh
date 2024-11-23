8c11f49d92ac4a66d16afec9fe2010b9224b1cb0
Denis Davydov <36914881+devldavydov@users.noreply.github.com>
2024-11-23 21:24:26 +0300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -4,3 +4,7 @@ cd PROJECT_DIR
 
 sqlite3 myfood.db ".backup 'myfood_$(date +%Y%m%d).db'"
 find . -type f -mtime +5 -name 'myfood_*.db' -execdir rm -- '{}' \;
+
+# Crontab example
+# m h  dom mon dow   command
+# 0 2 * * * PROJECT_DIR/backup.sh
